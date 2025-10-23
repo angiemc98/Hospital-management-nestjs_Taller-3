abd4efdd5807fbe3c24e39e9a344b2bdcc2ad590
angiemc98 <molinanatalia@hotmail.com>
2025-10-23 14:13:11 -0500

    feat: Creaciónde archivos dtos, desarrollo completo del modulo patient y verificación de endpoints

--- src/patient/patient.entity.ts
+++ src/patient/patient.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from "typeorm";
 import { Person } from "../person/person.entity";
 
 
@@ -9,8 +9,14 @@ export class Patient {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Column({type: 'varchar', length: 50})
+    bloodType: string;
+
+    @Column({type: 'text', nullable: true})
+    medicalHistory: string;
 
     //Relationships
     @OneToOne(() => Person, (person) => person.patient, {cascade: true})
+    @JoinColumn({name:'person_id'})
     person: Person;
 }
